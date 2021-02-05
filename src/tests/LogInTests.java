2bd22ca7e50c9eaaeed723fee8919d7804bdb990
jelenabrkic <jelena.brkic18@gmail.com>
2021-02-05 21:51:04 +0100

    Infrastucture for tests added

---
+++ src/tests/LogInTests.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class LogInTests extends TestBase {
+	
+	@Test
+	public void logIn() throws InterruptedException {
+		driver.navigate().to("http://cms.demo.katalon.com/");
+		Thread.sleep(3000);
+	}
+	
+}
